4a70383064265a21a6afe7f42f0b207fc28c3a8f
sgn.niedes@gmail.com <sgniedes@up.edu.ph>
2026-05-17 19:32:50 +0800

    feat: add AuthRepository wrapping AuthService with typed AppUser returns

---
+++ test/repositories/auth_repository_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:isdex/repositories/auth_repository.dart';
+
+void main() {
+  group('AuthRepository', () {
+    test('class is constructable', () {
+      expect(AuthRepository, isNotNull);
+    });
+  });
+}
