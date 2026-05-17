491d4abd72f58227e4fbd54461d7501d461cd4a4
sgn.niedes@gmail.com <sgniedes@up.edu.ph>
2026-05-17 21:01:04 +0800

    fix: move RTDB persistence to app.dart initState between Firebase init and DB ref

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-
-  if (!kIsWeb) {
-    FirebaseDatabase.instance.setPersistenceEnabled(true);
-    FirebaseDatabase.instance.setPersistenceCacheSizeBytes(10 * 1024 * 1024);
-  }
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
